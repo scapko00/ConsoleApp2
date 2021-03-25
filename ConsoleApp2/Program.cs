a362b915ee6ff9b6893f3b37e89ae1496463386c
kmaravic <56218855+kmaravic@users.noreply.github.com>
2021-03-25 17:27:53 +0100

    Update Program.cs
    dodan ispis

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("novi test");
+            Console.WriteLine("dodatak");
+           
         }
     }
 }
